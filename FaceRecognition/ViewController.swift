6a8ab25417ce4e9ba523603753ae93f7bb6dc52d
Lammax <lammax@mail.ru>
2019-12-10 21:08:53 +0300

    Initial commit

--- FaceRecognition/ViewController.swift
+++ FaceRecognition/ViewController.swift
@@ -10,31 +10,39 @@ import UIKit
 import SceneKit
 import ARKit
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+class ViewController: UIViewController {
 
     @IBOutlet var sceneView: ARSCNView!
+    @IBOutlet weak var alertLabel: UILabel!
+    
+    private let planeWidth: CGFloat = 0.13
+    private let planeHeight: CGFloat = 0.06
+    private let nodeYPosition: Float = 0.022
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        guard ARFaceTrackingConfiguration.isSupported else {
+            alertLabel.text = "Face tracking is not supported on this device"
+
+            return
+        }
+        
+        alertLabel.text = ""
+
         // Set the view's delegate
         sceneView.delegate = self
         
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
+        let configuration = ARFaceTrackingConfiguration()
 
         // Run the view's session
         sceneView.session.run(configuration)
@@ -51,12 +59,51 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
 /*
     // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
+    
 */
+    
+    
+}
+
+extension ViewController: ARSCNViewDelegate {
+    
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        guard let device = sceneView.device else {
+            alertLabel.text = "Device - error"
+            return nil
+        }
+        
+        let faceGeometry = ARSCNFaceGeometry(device: device)
+        let faceNode = SCNNode(geometry: faceGeometry)
+        faceNode.geometry?.firstMaterial?.fillMode = .lines
+     
+        //MARK: add glasses
+        let glassesPlane = SCNPlane(width: planeWidth, height: planeHeight)
+        glassesPlane.firstMaterial?.isDoubleSided = true
+        glassesPlane.firstMaterial?.diffuse.contents = UIImage(named: "glasses0")
+
+        
+        let glassesNode = SCNNode()
+        glassesNode.geometry = glassesPlane
+        glassesNode.position.z = faceNode.boundingBox.max.z * 3 / 4
+        glassesNode.position.y = nodeYPosition
+
+        faceNode.addChildNode(glassesNode)
+        faceNode.geometry?.firstMaterial?.transparency = 0
+        
+        
+        
+        return faceNode
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard let faceAnchor = anchor as? ARFaceAnchor, let faceGeometry = node.geometry as? ARSCNFaceGeometry else {
+            alertLabel.text = "FaceAnchor - error"
+            return
+        }
+        
+        faceGeometry.update(from: faceAnchor.geometry)
+    }
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
